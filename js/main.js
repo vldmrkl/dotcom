87df4b2c29754dbb893d4f1721cfa0acc228d51c
Volodymyr <klymenkodp@gmail.com>
2020-03-11 02:13:13 -0400

    develop static website v1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(() => {
+  $('.internal-link').on('click', function(event) {
+    event.preventDefault();
+
+    // smoothly slide to the position of the link's destination
+    $('html, body').animate(
+      {
+        scrollTop: $($.attr(this, 'href')).offset().top - 70,
+      },
+      700,
+    );
+  });
+
+  $('.card-face').on('click', function(event) {
+    $(this).fadeOut('fast');
+    $(this)
+      .next('.card-back')
+      .fadeIn('slow');
+  });
+
+  $('.card-back').on('click', function(event) {
+    $(this).fadeOut('fast');
+    $(this)
+      .prev('.card-face')
+      .fadeIn('slow');
+  });
+});
