b5b5599e915540844ac40bfa03fa2e531eb23ccd
Volodymyr <klymenkodp@gmail.com>
2021-12-19 22:02:16 -0500

    set up tailwind

--- vldmrkl-remix/app/root.jsx
+++ vldmrkl-remix/app/root.jsx
@@ -1,14 +1,8 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
+import { LiveReload, Links } from 'remix';
+import styles from './tailwind.css';
 
-export function meta() {
-  return { title: "New Remix App" };
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
 }
 
 export default function App() {
@@ -16,15 +10,12 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
+        <title>Volodymyr Klymenko</title>
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        vldmrkl.com
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
   );
